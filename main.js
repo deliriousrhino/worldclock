e2c0458c42b7150b9ed2070c63752e8dfa34114f
Ryan OConnell <ryan@deliriousrhino.com>
2015-12-27 14:17:40 +1100

    updates

--- main.js
+++ main.js
@@ -3,7 +3,9 @@
  var scrollEndTimeout = false;
  var yourTime;
  var zoneNames;
- var minLength = 2400 / (24 * 60); // px length of a min;
+ var zoneWidth = 3552;
+ var totalHours = 37;
+ var minLength =96/60; // px length of a min;
  var comparetimeZones = [{
      name: 'Vancouver',
      zone: 'America/Vancouver',
@@ -44,15 +46,16 @@
  function buildTimeRow(timeZone) {
      var dom = ''
      dom += '<div class="zone-name" id="' + timeZone.zone.split('/').join('-') + '"></div>';
-     dom += '<div class="zone">';
+     dom += '<div class="zone" style="width:' + zoneWidth + '">';
 
      dom += '<div class="times">';
      var time = moment();
      time.hours(0);
+     time.hours(time.hours() - 6);
      time.minutes(0);
      var randomColourId = Math.round(Math.random() * (colours.length - 1));
      var colour = colours[randomColourId]
-     for (var i = 1; i < 26; i++) {
+     for (var i = 0; i < totalHours; i++) {
          dom += buildTimeCell(time, timeZone, colour);
          time.hours(time.hours() + 1);
 
@@ -93,11 +96,26 @@
      var times = document.getElementById('times');
      var dom = buildYourTime();
      dom += buildComparisons();
+     dom +='<div class="edit-button"></div>';
      times.innerHTML = dom;
      yourTime = document.getElementsByClassName('times')[0];
      zoneNames = document.getElementsByClassName('zone-name');
-     window.addEventListener('scroll', scroll);
-     updateTimes();
+    
+     setTimeout(scrollToNow,100);
+ }
+
+ function scrollToNow(){
+   
+    var time = moment();
+    var totalMins = ((time.hours())*60)+time.minutes();
+    console.log('time.hours()', time.hours())
+    console.log('totalMins',totalMins)
+     var halfWidth = window.innerWidth / 2;
+    var scrollPos = (6*60*minLength + (totalMins*minLength))- halfWidth
+    window.scrollTo(Math.round(scrollPos),0);
+
+   updateScroll();
+   // window.addEventListener('scroll', scroll);
  }
 
  function scroll() {
@@ -126,16 +144,19 @@
  }
 
  function updateTimes() {
-     var timeMinOffset = Math.round((window.scrollX-1200) / minLength);
+     var halfWidth = window.innerWidth / 2;
+     var timeMinOffset = Math.round((window.scrollX + halfWidth) / minLength);
      var time = moment();
      time.hours(0);
-     time.minutes(time.minutes()+timeMinOffset);
+     time.minutes(0);
+     time.hours(time.hours() - 6);
+     time.minutes(time.minutes() + timeMinOffset);
      var zones = comparetimeZones.concat([yourTimeZome]);
      for (var z = 0; z < zones.length; z++) {
          var timeZone = zones[z];
          var zomeNameDom = document.getElementById(timeZone.zone.split('/').join('-'));
          var timeStr = time.tz(timeZone.zone).format('h:mm a');
-         zomeNameDom.innerHTML = '<span class="name">'+timeZone.name+'</span><span class="time-str">'+timeStr+'</span>';
+         zomeNameDom.innerHTML = '<span class="name">' + timeZone.name + '</span><span class="time-str">' + timeStr + '</span>';
      };
  }
 
